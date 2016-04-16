d8163954e8f839f844fa7a2ae66df2b2ddadf5ab
daaysesantos <daaysesantos@gmail.com>
2016-04-15 21:22:50 -0300

    pesquisar com erros

--- SysOtica - Projeto C#/SysOtica/SysOticaForm/frmListarClientes.cs
+++ SysOtica - Projeto C#/SysOtica/SysOticaForm/frmListarClientes.cs
@@ -15,6 +15,8 @@ namespace SysOticaForm
 {
     public partial class frmListarClientes : Form
     {
+        List<Cliente> listaCliente;
+       
         public frmListarClientes()
         {
             InitializeComponent();
@@ -25,7 +27,9 @@ namespace SysOticaForm
             //Desabilito a geração automática de colunas
             dataGridViewCliente.AutoGenerateColumns = false;
             //Atribuo o list retornado pelo método ao DataSource do grid
-            dataGridViewCliente.DataSource = new ClienteDados().listaCliente();
+            listaCliente = new ClienteDados().listaCliente();
+            dataGridViewCliente.DataSource = listaCliente;
+            
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
@@ -35,28 +39,9 @@ namespace SysOticaForm
 
         private void dataGridViewCliente_MouseClick(object sender, MouseEventArgs e)
         {
-            if (dataGridViewCliente.Rows.Count > 1)
+            if (dataGridViewCliente.SelectedRows[0].Index >= 0)
             {
-                dataGridViewCliente.SelectedRows[0].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[1].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[2].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[3].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[4].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[5].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[6].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[7].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[8].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[9].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[10].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[11].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[12].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[13].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[14].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[15].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[16].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[17].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[18].Cells[0].Value.ToString();
-                dataGridViewCliente.SelectedRows[19].Cells[0].Value.ToString();
+                Cliente clienteSelecionado = listaCliente.ElementAt(dataGridViewCliente.SelectedRows[0].Index);
             }
             else
             {
@@ -68,7 +53,7 @@ namespace SysOticaForm
         {
             Cliente cliente = new Cliente();
             IClienteDados dc = new ClienteDados();
-            string cl_cpf = maskedTextBoxCPF.Text;
+            string cl_cpf = textBoxPesqCPF.Text;
             dataGridViewCliente.DataSource = dc.pesquisarCliente(cl_cpf);
 
         }
